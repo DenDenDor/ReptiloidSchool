8fa74ff3dd70a96d4046973c57f62ff30db175cc
Juno <dendor333@gmail.com>
2023-11-05 14:01:09 +0300

    Revert "Not working shit"
    This reverts commit a1a334fcad2c3039ca2f41bb7d12ff6e2c3f6e84.

--- Reptiloid Schoolchildren/Assets/Scripts/Message/Answer/TeacherAnswer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "TeacherAnswer", menuName = "ScriptableObjects/Answer/TeacherAnswer")]
-public class TeacherAnswer : Answer
-{
-    public override Color AnswerColor => Color.white;
-
-}
--- Reptiloid Schoolchildren/Assets/Scripts/MovementController.cs
+++ Reptiloid Schoolchildren/Assets/Scripts/MovementController.cs
@@ -1,7 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Movement : MonoBehaviour { public float speed = 7f; public float MoveX; public float MoveY; void Update() { Rigidbody2D rb; rb = GetComponent<Rigidbody2D>(); MoveX = Input.GetAxis("Horizontal"); MoveY = Input.GetAxis("Vertical"); rb.velocity = new Vector2(MoveX * speed, MoveY * speed); } }
 
-//Написать @Taksisha
+public class Movement : MonoBehaviour
+{
+    public float speed = 7f;
+    public float Move = 40f;
 
+    void Update()
+    {
+        Rigidbody2D rb;
+        rb = GetComponent<Rigidbody2D>();
+
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+    }
+}
